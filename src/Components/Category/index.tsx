da0f91ac3ae922e8ff527bf059687f2300f438d3
Alex-Weise <Sannek65@gmail.com>
2022-07-01 16:36:17 +0300

    Адаптив для поиска

--- src/Components/Category/index.tsx
+++ src/Components/Category/index.tsx
@@ -24,7 +24,6 @@ const Category = () => {
     const [category, setCat] = useState<string[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState(false);
-    const [imgSRC, setImgSRC] = useState<string[]>([]);
     const [countPage, setCountPage] = useState(1);
     const [maxPage, setMaxPage] = useState(1);
 
@@ -45,17 +44,23 @@ const Category = () => {
         return () => setCat([]);
     }, [countPage, maxPage])
 
-    useEffect( () => {
-       category.map( async (item) => {
-          return await fetch(`https://dummyjson.com/products/category/${item}`)
-          .then(response => response.json())
-          .then(data => setImgSRC( prev => prev.concat(data.products[0].images[1])))
-          .catch(() => setIsError(true))
-          .finally(() => setIsLoading(false)); 
-       });
+    // useEffect( () => {
+    //    category.map( async (item) => {
+    //       return await fetch(`https://dummyjson.com/products/category/${item}`)
+    //       .then(response => response.json())
+    //       .then(data => setImgSRC( prev => prev.concat(data.products[0].images[1])))
+    //       .catch(() => setIsError(true))
+    //       .finally(() => setIsLoading(false)); 
+    //    });
 
-       return () => setImgSRC([])
-    }, [category])
+    //    return () => setImgSRC([])
+    // }, [category])
+    // const loadIMG = async (str:string) => {
+    //   const response = await fetch(`https://dummyjson.com/products/category/${str}`);
+    //   const data = await response.json();
+    //   const urlIMG = data.products[0].images[1];
+    //   setImgSRC(urlIMG);
+    // };
 
     const handlePageChange = (event:React.ChangeEvent<unknown>, page:number) => {
         event.preventDefault();
@@ -74,12 +79,11 @@ const Category = () => {
              category.map( (item, index) => {
                 let cat = item.slice(0,1).toUpperCase() + item.slice(1);
                 return (
-                <motion.div key={index} className={style.images}
+                <motion.div key={index} className={style.text}
                   variants={variantMotion}
                   initial='hidden'
                   animate='visible'
                   custom={index}>
-                    {/* <img src={imgSRC[index]} alt={item} className={style.img} /> */}
                     <Link to={`/category/${item}`} className={style.link}>{cat}</Link>
                 </motion.div>)
             })
@@ -89,3 +93,37 @@ const Category = () => {
     )
 }
 export {Category};
+
+
+// const loadAndIMG = async () => {
+//   try {
+//   const response = await fetch('https://dummyjson.com/products/categories');
+//   const data = await response.json();
+//   setMaxPage(Math.ceil(data.length / DEFAULT_LIMIT_CAT));
+//   for (let i = 1; i <= maxPage; i++ ) {
+//     if (i === countPage && i === 1) {
+//       const arr = data.slice(0, DEFAULT_LIMIT_CAT);
+//         arr.map( async (item:string) => {
+//          return await fetch(`https://dummyjson.com/products/category/${item}`)
+//           .then(response => response.json())
+//           .then(data => setImgSRC( prev => prev.concat(data.products[0].images[1])))
+//           .catch(() => setIsError(true))
+//           .finally(() => setIsLoading(false)); 
+//        });
+//       return setCat( data.slice(0, DEFAULT_LIMIT_CAT))
+//     }
+//     else if (i === countPage) {
+//       const arr = data.slice(0, DEFAULT_LIMIT_CAT);
+//       arr.map( async (item:string) => {
+//        return await fetch(`https://dummyjson.com/products/category/${item}`)
+//         .then(response => response.json())
+//         .then(data => setImgSRC( prev => prev.concat(data.products[0].images[1])))
+//         .catch(() => setIsError(true))
+//         .finally(() => setIsLoading(false)); 
+//      });
+//       return setCat( data.slice(DEFAULT_LIMIT_CAT * (i - 1), DEFAULT_LIMIT_CAT * i))}}
+//   } 
+//   catch(err) { setIsError(true)}
+//   finally { setIsLoading(false)};
+
+// }
--- src/Components/Search/index.tsx
+++ src/Components/Search/index.tsx
@@ -1,7 +1,9 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import SearchIcon from '@mui/icons-material/Search';
 import style from "./styles.module.scss";
 import { useNavigate } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";  
 
 type TProp = {
   value?: string,
@@ -10,6 +12,9 @@ type TProp = {
 const Search:FC<TProp> = ({value}) => {
   const navigate = useNavigate();
   let search = "";
+  const [isVisible, setIsVisible] = useState(true);
+  
+  const handleVisibility = () => setIsVisible(!isVisible);
 
   const handleSubmit = (event:any) => {
         event.preventDefault();
@@ -19,14 +24,18 @@ const Search:FC<TProp> = ({value}) => {
   };
 
     return ( 
-      <section className={style.search}>
-        <form onSubmit={handleSubmit}>
+      <section className={isVisible ? style.search : style.searchHidden}>
+        <form onSubmit={handleSubmit} className={!isVisible && style.hidden}>
           <label className={style.label}>
                 Что ищем? <input name="search" className={style.input}></input>
           </label>
           <button type="reset" className={style.reset}>X</button>
           <button type="submit" className={style.button}>Искать <SearchIcon sx={{color: "grey", fontSize: 18}}/></button>
+          <button type="button" className={style.close} onClick={handleVisibility}><ArrowForwardIosIcon fontSize="inherit" /></button>
         </form>
+        <button type="button" className={!isVisible ? style.open : style.hidden } onClick={handleVisibility}>
+          <SearchIcon fontSize='inherit' />
+        </button>
     </section>)
 }
 
